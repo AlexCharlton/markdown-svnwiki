28cbd3549ca5ab4e2479a0d4110ba877423fe206
Alex Charlton <alex.n.charlton@gmail.com>
2014-04-17 14:47:21 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,92 @@
+# markdown-svnwiki
+Converts Markdown to the svnwiki syntax used on the [Chicken wiki](https://wiki.call-cc.org/edit-help). It uses [lowdown](http://wiki.call-cc.org/eggref/4/lowdown) to transform Markdown into SXML before transforming it into svnwiki with [sxml-transforms](http://wiki.call-cc.org/eggref/4/sxml-transforms). Much credit goes to those two libraries, particularly lowdown which heavily influenced the code in markdown-svnwiki.
+
+markdown-svnwiki includes pre and post-processing phases for performing customizable transformations on the input and output. Some transformations, meant to make working with the Chicken wiki more convenient, are included by default. They are described in the section Special Syntax.
+
+## Installation
+This repository is a [Chicken Scheme](http://call-cc.org/) egg.
+
+It is part of the [Chicken egg index](http://wiki.call-cc.org/chicken-projects/egg-index-4.html) and can be installed with `chicken-install markdown-svnwiki`.
+
+## Requirements
+* lowdown
+* sxml-transforms
+* miscmacros
+
+## Documentation
+
+    [procedure] (markdown->svnwiki input)
+
+Convert the given `input` (may be a string or a port) into svnwiki, outputting to `current-output-port`.
+
+    [parameter] pre-processing
+
+An alist of functions that accept a string (the input to `markdown->svnwiki`) and should transform it in some way. By default contains one entry: `code-blocks`, for dealing with code blocks as described in Special Syntax.
+
+    [parameter] post-processing
+An alist of functions that accept a string (the pre-post-processed output from `markdown->svnwiki`) and should transform it in some way. By default contains: `code-blocks`, `procedure`, `macro`, `read`, `parameter`, `record`, `string`, `class`, `method`, `constant`, `setter`, `syntax`, and `type`, for dealing with code blocks and definition tags as described in Special Syntax.
+
+    [parameter] toc
+
+If true, inserts `[[toc:]]` after the first-level heading(s). Defaults to `#t`.
+
+### Special syntax
+markdown-svnwiki supports the syntax highlighted code blocks that [GitHub](help.github.com/articles/github-flavored-markdown#syntax-highlighting), [Pandoc](http://johnmacfarlane.net/pandoc/README.html#fenced-code-blocks) and perhaps others support. It converts these blocks into the `<enscript>` tags that highlight with the given language. It does this using a pre and post-processing step, both named `code-blocks`. These code blocks take the following form:
+
+    ``` Scheme
+    code ...
+    ```
+
+Becomes:
+
+    <enscript highlight="scheme">
+    code ...
+    </enscript>
+
+Additionally, markdown-svnwiki supports a special syntax for adding the [definition tags](https://wiki.call-cc.org/edit-help#extensions-for-chicken-documentation) supported by the Chicken wiki. Single-line verbatim code blocks that begin with exactly four spaces followed by `[definition-type]` are given a `<definition-type>` tag. Only the tags supported by the Chicken wiki are supported. For example:
+
+        [procedure] (my-proc ...)
+
+Becomes:
+
+    <procedure>(my-proc ...)</procedure>
+
+## Examples
+This example can be compiled to make a command line program that accepts one argument - a Markdown file - and outputs a svnwiki file into the same directory. It shows the addition of of a pre-processing step - one that removes the first section called "Installation" (fairly indiscriminately, it stops at the first `#`).
+
+``` Scheme
+(import chicken scheme irregex)
+(use markdown-svnwiki)
+
+(define file-name (cadr (argv)))
+(define output-name (pathname-replace-extension file-name "svnwiki"))
+
+(pre-processing
+ (cons
+  (cons 'remove-installation
+        (lambda (s)
+          (irregex-replace "## Installation[^#]*" s "")))
+  (pre-processing)))
+
+(call-with-output-file output-name
+  (lambda (output)
+    (call-with-input-file file-name
+      (lambda (input)
+        (current-output-port output)
+        (markdown->svnwiki input)))))
+```
+
+## Version history
+### Version 0.1.0
+* Initial release
+
+## Source repository
+Source available on [GitHub](https://github.com/AlexCharlton/markdown-svnwiki).
+
+Bug reports and patches welcome! Bugs can be reported via GitHub or to alex.n.charlton at gmail.
+
+## Author
+Alex Charlton
+
+## License
+BSD
---
+++ markdown-svnwiki.scm
@@ -0,0 +1,157 @@
+(module markdown-svnwiki
+  (markdown->svnwiki
+   pre-processing
+   post-processing
+   toc)
+
+(import chicken scheme files irregex data-structures srfi-1 ports extras srfi-13)
+(use lowdown sxml-transforms miscmacros)
+
+(define references (make-parameter #f))
+
+(define toc (make-parameter #t))
+
+(define pre-processing
+  (make-parameter
+   `((code-blocks .
+      ,(lambda (s)  ; Deal with code blocks (``` lang ... ```)
+         (irregex-replace/all
+          '(: "\n```" (+ whitespace)
+              (submatch-named lang (+ alphanumeric))
+              (submatch-named body (*? any)) "\n```")
+          s
+          (lambda (m)
+            (string-append "\n    [lang:"
+                           (string-downcase (irregex-match-substring m 'lang))
+                           "]\n"
+                           (irregex-replace/all
+                            "\n" (irregex-match-substring m 'body) "\n    ")))))))))
+
+;; Convert verbatim blocks containing [definition-type] into <definition-type tags
+(define (definition name)
+  (cons (string->symbol name)
+        (lambda (s)
+          (irregex-replace/all
+           `(: "    [" ,name "]" (submatch-named def (+ (~ #\newline))) #\newline)
+           s
+           (lambda (m)
+             (string-append "\n<" name ">" (irregex-match-substring m 'def)
+                            "</" name ">\n\n"))))))
+
+(define post-processing
+  (make-parameter
+   `((code-blocks .
+      ,(lambda (s) ; Convert [lang:xxx] verbatim blocks into <enscript> tags
+         (irregex-replace/all
+          '(: "\n    [lang:" (submatch-named lang (+ alphanumeric)) "]\n"
+              (submatch-named body (+ bol "    " (* (~ #\newline)) #\newline)))
+          s
+          (lambda (m)
+            (string-append "\n<enscript highlight=\""
+                           (irregex-match-substring m 'lang)
+                           "\">\n"
+                           (irregex-replace/all
+                            "\n    " (irregex-match-substring m 'body) "\n")
+                           
+                           "</enscript>\n")))))
+     ,(definition "procedure")
+     ,(definition "macro")
+     ,(definition "read")
+     ,(definition "parameter")
+     ,(definition "record")
+     ,(definition "string")
+     ,(definition "class")
+     ,(definition "method")
+     ,(definition "constant")
+     ,(definition "setter")
+     ,(definition "syntax")
+     ,(definition "type"))))
+
+(define (call-with-reference attrs proc)
+  (if* (alist-ref (car (alist-ref 'ref attrs))
+                  (references) equal?)
+       (proc it attrs)
+       (alist-ref 'input attrs)))
+
+(define (ref->alist-entry ref)
+  (cons (car (alist-ref 'label (cdr ref)))
+        (cdr ref)))
+
+(define (make-link ref #!optional attrs)
+  `("[[" ,(alist-ref 'href ref)
+    ,(if* (alist-ref 'label (or attrs ref))
+          (list "|" it)
+          '())
+    "]]"))
+
+(define (make-image ref #!optional attrs)
+  `("[[image:" ,(alist-ref 'href ref)
+    ,(if* (alist-ref 'label (or attrs ref))
+          (list "|" it)
+          '())
+    "]]"))
+
+(define conversion-rules
+  `((heading . ,(lambda (_ attrs)
+                  (list #\newline
+                        (make-string (add1 (car attrs)) #\=)
+                        (cadr attrs)
+                        #\newline
+                        (if (and (= (car attrs) 1) (toc))
+                            '("[[toc:]]" #\newline)
+                            '()))))
+    (paragraph . ,(lambda (_ attrs)
+                    `(,attrs #\newline #\newline)))
+    (explicit-link . ,(lambda (_ attrs)
+                        (make-link attrs)))
+    (reference-link . ,(lambda (_ attrs)
+                         (call-with-reference attrs make-link)))
+    (auto-link . ,(lambda (_ attrs)
+                    `("[[" ,(alist-ref 'href attrs) "]]")))
+    (image . ,(lambda (_ attrs)
+                (make-image attrs)))
+    (reference-image . ,(lambda (_ attrs)
+                          (call-with-reference attrs make-image)))
+    (bullet-list . ,(lambda (_ items)
+                      (map (lambda (s) (cons "*" s)) items)))
+    (ordered-list . ,(lambda (_ items)
+                       (map (lambda (s) (cons "#" s)) items)))
+    (item . ,(lambda (_ contents)
+               `(#\space ,@contents #\newline)))
+    (verbatim . ,(lambda (_ attrs)
+                   (map (lambda (s)
+                          (map (lambda (s) (list "    " s)) s))
+                        attrs)))
+    (code . ,(lambda (_ attrs)
+               `("{{" ,@attrs "}}")))
+    (emphasis . ,(lambda (_ text)
+                   `("''" @,text "''")))
+    (strong . ,(lambda (_ text)
+                 `("'''" @,text "'''")))
+    (html-element . ,(lambda (_ contents)
+                       contents))
+    (comment . ,(lambda (_ contents)
+                  (list #\< "!--" contents "--" #\> #\newline)))
+    . ,alist-conv-rules*))
+
+(define (markdown->svnwiki input)
+  (define (reference-element? el)
+    (and (pair? el) (eq? 'reference (car el))))
+  (define (process funs string)
+    (let loop ([str string] [processing funs])
+      (if (null? processing)
+          str
+          (loop ((cdar processing) str)
+                (cdr processing)))))
+  (let* ([input (if (port? input)
+                    (read-string #f input)
+                    input)]
+         [string (process (pre-processing) input)])
+    (receive (refs sxml) (partition reference-element? (markdown->sxml* string))
+      (parameterize ((references (map ref->alist-entry refs)))
+        (write-string (process (post-processing)
+                               (with-output-to-string
+                                 (lambda ()
+                                   (SRV:send-reply
+                                    (pre-post-order* sxml conversion-rules))))))))))
+) ;module end
